f75d599a9e4c2f69f8f777a04b9b026198880bbc
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-09-21 11:55:27 -0500

    I've done the other two pages now, contact us and about us. They were a lot easier. I think it might be done. At least the bulk of it is definitely done. I'd like to move on to more topics now.

--- src/pageScripts/aboutUs.js
+++ src/pageScripts/aboutUs.js
@@ -1,1 +1,27 @@
-export const renderAboutUs = () =>{};
+export const renderAboutUs = () =>{
+    //retrieve and clear the content
+    const content = document.querySelector("#content");
+    content.innerHTML = "";
+
+    const paragraph = "Welcome to The Hot Spot, where every dish tells a story of flavor, family, and tradition. Our journey began not in a professional kitchen, but in the vibrant backyard of our founder, AK, whose legendary weekend cookouts became the stuff of local legend. Friends and neighbors would flock for a taste of his fiery jerk chicken, slow-braised oxtail that fell off the bone, and perfectly seasoned fried chicken. The demand grew so great that what started as a passionate hobby blossomed into a dream. We opened our doors to bring that same authentic, soul-satisfying Caribbean and Southern comfort food to everyone. Today, we honor those humble beginnings, using only the freshest ingredients, secret family recipes, and cooking everything with the same love that started it all. This is more than a restaurant; it’s our home, and you’re always invited to the table."
+
+
+    const aHero = document.createElement("div");
+    aHero.classList.add("aboutUsHero");
+    content.appendChild(aHero);
+
+    const aInfo = document.createElement("div");
+    aInfo.classList.add("aboutUsInfo");
+    aHero.appendChild(aInfo);
+
+    const aTitle = document.createElement("h1");
+    aTitle.textContent = "About Us";
+    const aPara = document.createElement("p");
+    aPara.textContent = paragraph;
+
+    aInfo.appendChild(aTitle);
+    aInfo.appendChild(aPara);
+
+
+
+};
--- src/pageScripts/contactUs.js
+++ src/pageScripts/contactUs.js
@@ -1,1 +1,20 @@
-export const renderContactUs = () =>{};
+export const renderContactUs = () =>{
+    //retrieve and clear the content
+    const content = document.querySelector("#content");
+    content.innerHTML = "";
+
+    const contactContainer = document.createElement("div");
+    contactContainer.classList.add("contactContainer");
+    content.appendChild(contactContainer);
+
+    const cBox = document.createElement("div");
+    cBox.classList.add("contactBox");
+    contactContainer.appendChild(cBox);
+
+    const cTitle = document.createElement("h2");
+    cTitle.textContent = "Contact Us";
+    const cInfo = document.createElement("p");
+    cInfo.innerHTML = "Phone: (876)393-5091<br/><br/>Email: akHotSpot33@gmail.com";
+    cBox.appendChild(cTitle);
+    cBox.appendChild(cInfo);
+};
